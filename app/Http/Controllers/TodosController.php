b1bf14f52cdaf055059a50dc1c9ffcbd0320f645
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-28 12:02:50 +0900

    feat: Todosコントローラを必要なメソッドのみに絞った, 各メソッド完成

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -7,58 +7,96 @@ use App\Todo;
 
 class TodosController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return Todo::all()->toJson();
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $user = User::where('hash_id', $request->header('uid'))->first();
+
+    if (!$user) {
+      return response('User not found', 404);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    $todo = new Todo;
+    $todo->title = $request->title;
+    $todo->user_id = $user->id;
+    $todo->save();
+    return response(201)->json(Todo::find($todo->id));
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $todo = Todo::find($request->id)->first();
+
+    if (!$todo) {
+      return response('Todo not found', 404);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    return $todo->toJson();
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $user = User::where('hash_id', $request->header('uid'))->first();
+    if (!$user) return response('Author not found', 404);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    $todo = Todo::find($id)->first();
+    if (!$todo) return response('Todo not found', 404);
+
+    // ログインユーザーが投稿者自身でなければアクセス不可
+    if ($user->id === $todo->user_id) return response(401);
+
+    $todo->title = $request->title;
+    $todo->isDone = $request->isDone;
+    $todo->save();
+    return response(204)->json($todo);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $user = User::where('hash_id', $request->header('uid'))->first();
+    if (!$user) return response('Author not found', 404);
+
+    $todo = Todo::find($id)->first();
+    if (!$todo) return response('Todo not found', 404);
+
+    // ログインユーザーが投稿者自身でなければアクセス不可
+    if ($user->id === $todo->user_id) return response(401);
+
+    Todo::destroy($id);
+    return response(204)->json($todo);
+  }
 }
