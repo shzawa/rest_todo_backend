dbf5690e429bf03e84c9e9997b855e9388ab17c9
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-28 10:59:38 +0900

    fix: todosマイグレーションのuser_idの型を修正

--- database/migrations/2020_04_28_014640_create_todos_table.php
+++ database/migrations/2020_04_28_014640_create_todos_table.php
@@ -17,7 +17,7 @@ class CreateTodosTable extends Migration
             $table->id();
             $table->string('title');
             $table->boolean('isDone')->nullable()->default(false);
-            $table->integer('user_id');
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
