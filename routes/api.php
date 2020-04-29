c1713a80254f412292a96e0e49089e9f7fcd8a48
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-29 13:45:30 +0900

    update: usersコントローラへのルーティング追加, group整理

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'api'], function () {
-  Route::resource('todos', 'TodosController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
+  Route::group(['prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'auth'], function () {
+      Route::post('/sign_up', 'UsersController@store')->name('auth.sign_up');
+      Route::post('/sign_in', 'UsersController@login')->name('auth.sign_in');
+      Route::delete('/resign', 'UsersController@destroy')->name('auth.resign');
+    });
+
+    Route::group(['prefix' => 'users'], function () {
+      Route::get('/', 'UsersController@index')->name('users.index');
+      Route::get('/{id}', 'UsersController@show')->name('users.show');
+      Route::put('/{id}', 'UsersController@update')->name('users.update');
+      Route::delete('/{id}', 'UsersController@destroy')->name('users.destroy');
+    });
+
+    Route::resource('todos', 'TodosController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
+  });
 });
