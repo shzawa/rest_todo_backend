28cda3aa281214cde5a8cae0dd39f68e1f8015b6
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-29 13:46:04 +0900

    fix: Userモデルの継承先を記述, 利用可能なカラムを追加

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User
+class User extends Model
 {
   /**
    * The attributes that are mass assignable.
@@ -12,7 +12,7 @@ class User
    * @var array
    */
   protected $fillable = [
-    'name', 'email', 'password',
+    'name', 'email', 'password', 'hash_id'
   ];
 
   public function todos()
